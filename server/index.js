c81b3d871b6409ff2c41449cc647bfc92b3f3909
jsmithb117 <jsmithb117@gmail.com>
2021-03-09 10:26:17 -0800

    Server responds to a GET request for one image

--- server/index.js
+++ server/index.js
@@ -1,38 +1,43 @@
 const express = require('express');
+const db = require('../db/index.js');
+var bodyParser = require('body-parser');
 
 const app = express();
 const port = 3006;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client/dist'));
 
-
-// GET /api/image/:courseNumber/:imageName
-
-app.get('/api/image/:courseNumber/:imageName', (req, res, next) => {
-})
+app.get('/api/image/:courseNumber/:imageName', function (req, res) {
+  let imagesData = db.findOneImage(req.params.courseNumber, req.params.imageName, (image) => {
+    res.send(image);
+  });
+});
 
 // GET /api/primaryInstructor/:courseNumber
 
-app.get('/api/primaryInstructor/:courseNumber', (req, res, next) => {
+// app.get('/api/primaryInstructor/:courseNumber', (req, res, next) => {
 
-})
+// })
 
-// GET /api/images/:courseNumber
+// // GET /api/images/:courseNumber
 
-app.get('GET /api/images/:courseNumber', (req, res, next) => {
+// app.get('GET /api/images/:courseNumber', (req, res, next) => {
 
-})
+// })
 
-// GET /api/svg/:svgName
+// // GET /api/svg/:svgName
 
-app.get('/api/svg/:svgName', (req, res, next) => {
+// app.get('/api/svg/:svgName', (req, res, next) => {
 
-})
+// })
 
-// GET /api/svgs
+// // GET /api/svgs
 
-app.get('/api/svgs', (req, res, next) => {
+// app.get('/api/svgs', (req, res, next) => {
 
-})
+// })
 
 app.listen(port, () => {
   console.log(`Images service listening at http://localhost:${port}`);
