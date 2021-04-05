d7b5c376094653f027ef0ab0d20211bcf4718887
jsmithb117 <jsmithb117@gmail.com>
2021-04-05 09:54:46 -0700

    Changed urls for seed script

--- db/seed/imagesSeed.js
+++ db/seed/imagesSeed.js
@@ -1,39 +1,39 @@
-const faker = require('faker');
+/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-restricted-syntax */
+// const faker = require('faker');
 const fs = require('fs');
-const imagesModel = require('./models/imagesModel');
 const mongoose = require('mongoose');
-const path = require('path');
+// const path = require('path');
 const fetch = require('node-fetch');
+const imagesModel = require('./models/imagesModel');
+
 const instructorsPort = 3003;
 
 const generateImages = () => {
-  let images = [];
+  const images = [];
   const offeredBys = ['DeepLearning', 'EURA', 'IBM', 'Illinois', 'Penn', 'UVA'];
   let instructors = [];
   let offeredBysData = [];
 
-  fetch(`http://localhost:${instructorsPort}/api/allinstructors`)
-    .then((instructorsResponse) => {
-      return instructorsResponse.json();
-    })
+  fetch(`http://127.0.0.1:${instructorsPort}/api/allinstructors`)
+    .then((instructorsResponse) => instructorsResponse.json())
     .then((instructorsJSON) => {
       instructors = instructorsJSON;
-      return fetch(`http://localhost:${instructorsPort}/api/offeredByAll`)
+      return fetch(`http://127.0.0.1:${instructorsPort}/api/offeredByAll`);
     })
-    .then((offeredByResponse) => {
-      return offeredByResponse.json();
-    })
+    .then((offeredByResponse) => offeredByResponse.json())
     .then((offeredByJSON) => {
       offeredBysData = offeredByJSON;
     })
     .then(() => {
-      //uses instructors and offeredBy data to create image data for each course
-      let primaryInstructorsObj = {}; //{courseNumber: primaryInstructor.id}
-      let additionalInstructorsObj = {};//{courseNumber: [additionalInstructor1.id, additionalInstructor2.id...]}
+      // uses instructors and offeredBy data to create image data for each course
+      const primaryInstructorsObj = {}; // {courseNumber: primaryInstructor.id}
+      const additionalInstructorsObj = {};// {courseNumber: [additionalInstructor1.id...]}
       for (let i = 1; i <= 100; i++) {
         additionalInstructorsObj[i] = [];
       }
-      for (let instructor of instructors) {
+      for (const instructor of instructors) {
         for (let i = 0; i < instructor.courses.length; i++) {
           if (instructor.courses[i].isPrimaryInstructor) {
             primaryInstructorsObj[instructor.courses[i].courseNumber] = instructor.id;
@@ -43,38 +43,38 @@ const generateImages = () => {
         }
       }
 
-      //creates 100 image documents
+      // creates 100 image documents
       for (let id = 1; id <= 100; id++) {
-        let offeredBy = offeredBysData[id - 1].offeredByIndex;
+        const offeredBy = offeredBysData[id - 1].offeredByIndex;
 
-        //builds additionalInstructors array for each course
-        let additionalInstructors = [];
+        // builds additionalInstructors array for each course
+        const additionalInstructors = [];
         for (let i = 0; i < additionalInstructorsObj[id].length; i++) {
-          let URL = {
+          const URL = {
             instructorId: additionalInstructorsObj[id][i],
-            instructorImage: `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/instructors/${additionalInstructorsObj[id][i]}.jpg`
+            instructorImage: `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/instructors/${additionalInstructorsObj[id][i]}.jpg`,
           };
           additionalInstructors.push(URL);
         }
 
-        //assigns 3 of 9 testimonials to each course
-        let testimonialsArray = [];
+        // assigns 3 of 9 testimonials to each course
+        const testimonialsArray = [];
         while (testimonialsArray.length < 3) {
-          let random = Math.ceil(Math.random() * 9);
+          const random = Math.ceil(Math.random() * 9);
           if (!testimonialsArray.includes(random)) {
             testimonialsArray.push(random);
           }
         }
-        let testimonial1Id = testimonialsArray[0];
-        let testimonial2Id = testimonialsArray[1];
-        let testimonial3Id = testimonialsArray[2];
+        const testimonial1Id = testimonialsArray[0];
+        const testimonial2Id = testimonialsArray[1];
+        const testimonial3Id = testimonialsArray[2];
 
-        let testimonial1Image = `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/testimonials/${testimonialsArray[0]}.jpg`;
-        let testimonial2Image = `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/testimonials/${testimonialsArray[1]}.jpg`;
-        let testimonial3Image = `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/testimonials/${testimonialsArray[2]}.jpg`;
+        const testimonial1Image = `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/testimonials/${testimonialsArray[0]}.jpg`;
+        const testimonial2Image = `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/testimonials/${testimonialsArray[1]}.jpg`;
+        const testimonial3Image = `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/testimonials/${testimonialsArray[2]}.jpg`;
 
-        //brings together each 'image' then pushes to images
-        let image = {
+        // brings together each 'image' then pushes to images
+        const image = {
           id,
           offeredBy,
           offeredByAbout: `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/offeredByAbouts/${offeredBys[offeredBy]}About.png`,
@@ -87,19 +87,19 @@ const generateImages = () => {
           testimonial2Id,
           testimonial3Image,
           testimonial3Id,
-          courseIcon: `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/courseIcons/${id}.png`
+          courseIcon: `https://rpt26-ingenuity.s3-us-west-1.amazonaws.com/courseIcons/${id}.png`,
         };
         images.push(image);
       }
-      //inserts completed images data into images table of images database
+      // inserts completed images data into images table of images database
       imagesModel.insertMany(images, (err) => {
         if (err) {
           console.error(err);
         }
         console.log('imagesInsert success');
         mongoose.connection.close();
-      })
-      //writes data to images.json (no longer necessary, but acts as a reference for development and testing)
+      });
+      // write data to images.json(acts as a reference for development, testing)
       fs.writeFileSync('./db/seed/data/images.json', JSON.stringify(images, null, '\t'));
     })
     .catch((err) => {
@@ -107,7 +107,6 @@ const generateImages = () => {
         console.error('Error in generateImages: ', err);
       }
     });
-  return;
 };
 
 generateImages();
